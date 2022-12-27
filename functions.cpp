a991b744739ff2807cd5670e714f9803fabaf522
GalinaS3 <116189685+GalinaS3@users.noreply.github.com>
2022-12-27 19:39:40 +0500

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,76 @@
+﻿#include"functions.h"
+
+
+void badCharHeuristic(const string& str, int size, int badchar[NO_OF_CHARS])
+{
+    int i;
+
+    for (i = 0; i < NO_OF_CHARS; i++)
+        badchar[i] = -1;
+
+    for (i = 0; i < size; i++)
+        badchar[(int)str[i]] = i;
+}
+
+void IsKPeriodic(const string& str,  int K)
+{    
+    if (K <= 0)
+    {
+        cout << "К должно быть больше нуля" << '\n';
+        return;
+    }
+
+    int l = 0;
+    int counter = 0;
+    int n = str.size();
+
+    if (K >= n)
+    {
+        cout << "К больше либо равно длине строки" << '\n';
+        return;
+    }
+
+    if (n % K)
+    {
+        cout << "Строка длины " << n << " не может быть кратна " << K << '\n';
+        return;
+    }
+
+    string substr;
+    substr.assign(str, 0, K);
+    int m = substr.size();
+    int badchar[NO_OF_CHARS];
+
+    badCharHeuristic(substr, m, badchar);
+
+    int s = 0;
+    while (s <= (n - m))
+    {
+        int j = m - 1;
+
+        while (j >= 0 && substr[j] == str[s + j])
+            j--;
+
+        if (j < 0)
+        {
+            counter++;
+            if (s != 0 && s - l == K)
+                l = s;
+            if (!s)
+                l = s;
+            s += (s + m < n) ? m - badchar[str[s + m]] : 1;
+        }
+        else
+        {
+            int bc = badchar[str[s + j]];
+            s += max(1, j - bc);
+        }
+    }
+    if (counter && str.size() - l == K)
+        cout << "Строка " << str << " кратна " << K << '\n';
+    else
+    {
+        if (counter)
+            cout << "Строка " << str << " не кратна " << K << '\n';
+    }
+ }
---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#include "functions.h"
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    
+    string str1 = "foobarfoobafoobar";
+
+    int K1 = 6;
+    IsKPeriodic(str1, K1);
+        
+    int K2 = -5;
+    IsKPeriodic(str1,  K2);
+
+    string str2 = "abcabcabcabcabc";
+
+    int K3 = 3;
+    IsKPeriodic(str2, K3);
+
+    int K4 = 5;    
+    IsKPeriodic(str2, K4);
+
+    int K5 = 100;
+    IsKPeriodic(str2,  K5);
+
+    int K6 = 4;
+    IsKPeriodic(str2, K6);
+
+    return 0;
+}
